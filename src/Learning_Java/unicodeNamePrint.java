77adad855668785ca73045ffdc6833ece87740f9
Mukesh <Mukesh@Maverick>
2023-11-08 12:21:45 +0530

    Printing name in Unicode..........

---
+++ src/Learning_Java/unicodeNamePrint.java
@@ -0,0 +1,21 @@
+package Learning_Java;
+
+public class unicodeNamePrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char c1 = '\u092E';
+		char c2 = '\u0941';
+		char c3 = '\u0958';
+		char c4 = '\u0947';
+		char c5 = '\u0936';
+		char c6 = '\u0913';
+		char c7 = '\u091D';
+		char c8 = '\u093B';
+		System.out.println(c1+""+c2+""+c3+""+c4+""+c5+" "+c6+""+c7+""+c8);
+		
+
+	}
+
+}
