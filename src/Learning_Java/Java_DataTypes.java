3e3ec0d629d11b3cb29b43d54eac6e8e656f86cd
Mukesh <Mukesh@Maverick>
2023-11-08 11:58:00 +0530

    Java Data Types...!!

---
+++ src/Learning_Java/Java_DataTypes.java
@@ -0,0 +1,47 @@
+package Learning_Java;
+
+public class Java_DataTypes {
+
+	public static void main(String[] args) {
+		/* ********Primitive Data Types********************
+		   -----------Numeric Types----------------
+		   byte - 1byte      
+		   short - 2 bytes
+		   int - 4 bytes
+		   long -  8 bytes
+		   
+		   -----------Floating Types----------------
+		   float-  4 bytes     12.1f
+		   double - 8 bytes    12.10d
+		   
+		   -----------Character Type----------------
+		    char -  ASCII - 1 byte
+		            UNI Code - 2 bytes
+		            
+		   -----------Boolean Type----------------
+		    boolean - 1bit    True/false       
+		*/
+		
+		
+		byte b1 = 100;
+		short s1 = 150;
+		int i1 = 1;
+		long l1 = 23;
+		float f1=12.34f;
+		double d1=12.45D;
+		char c1 = 'A';
+		boolean b2= true;
+		
+		System.out.println("Byte value : "+b1);
+		System.out.println("Short value : "+s1);
+		System.out.println("int value : "+i1);
+		System.out.println("long value : "+l1);
+		System.out.println("float value : "+f1);
+		System.out.println("double value : "+d1);
+		System.out.println("Character value in ASCII: "+c1);
+		System.out.println("Boolean value : "+b2);
+		
+
+	}
+
+}
