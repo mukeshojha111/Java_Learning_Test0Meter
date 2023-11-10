16c9220ead16c3e5460c08d093ec53922791b6b4
Mukesh Ojha <Mukesh Ojha@MUKESH-PF2XRWFT-82lN>
2023-11-10 11:42:14 +0530

    Greater Number...

---
+++ src/Learning_Java/GreaterNumbers.java
@@ -0,0 +1,28 @@
+package Learning_Java;
+
+import java.util.Scanner;
+
+public class GreaterNumbers {
+
+	public static void main(String[] args) {
+
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.println("Enter the three numbers..");
+		
+		int num1 = userInput.nextInt();
+		int num2 = userInput.nextInt();
+		int num3 = userInput.nextInt();
+		
+		if(num1>num2 && num1 >num3)
+		{
+			System.out.println(num1+" is Greater..");
+		}
+		else if(num2>num1 && num2 >num3)
+		{
+			System.out.println(num2+" is Greater..");
+		}
+
+	}
+
+}
