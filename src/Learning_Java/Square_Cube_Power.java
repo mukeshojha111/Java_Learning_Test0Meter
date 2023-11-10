72d6d7bcf7c89cbea863d69efa938ace0976674e
Mukesh Ojha <Mukesh Ojha@MUKESH-PF2XRWFT-82lN>
2023-11-10 11:10:39 +0530

    square/cube calculation

---
+++ src/Learning_Java/Square_Cube_Power.java
@@ -0,0 +1,27 @@
+package Learning_Java;
+
+import java.util.Scanner;
+
+public class Square_Cube_Power {
+
+	public static void main(String[] args) {
+		
+        Scanner userInput = new Scanner(System.in);
+		
+	    System.out.println("Enter the Number to calculate Square,Cube and 4th Power...");
+	    
+	    double number = userInput.nextDouble();
+	    
+	    double squareOfNumber = number * number;
+	    
+	    double cubeOfNumber = number * number * number;
+	    
+	    double forthPowOfNumber = number * number * number  * number;
+	    
+	    System.out.println("Square: "+squareOfNumber);
+	    System.out.println("Cube: "+cubeOfNumber);
+	    System.out.println("Forth Power: "+forthPowOfNumber);
+
+	}
+
+}
