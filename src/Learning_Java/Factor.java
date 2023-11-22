4cf99d3fe3b88f49568baf909cf4faa0f2edebc1
Mukesh Ojha <Mukesh Ojha@MUKESH-PF2XRWFT-82lN>
2023-11-22 15:46:10 +0530

    updated..!!

--- src/Learning_Java/Factor.java
+++ src/Learning_Java/Factor.java
@@ -1,10 +1,14 @@
 package Learning_Java;
 
+import java.util.Scanner;
+
 public class Factor {
 
 	public static void main(String[] args) {
 		
-		int num = 10;
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Enter the Number for which you want Factors");		
+		int num = userInput.nextInt();
 		
 		System.out.println("Factors of Number "+num+" are..");
 		for(int i=1; i<=num; i++)
