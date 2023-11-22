17952813196c46f6033fb9e00ae4ec0217b69952
Mukesh Ojha <Mukesh Ojha@MUKESH-PF2XRWFT-82lN>
2023-11-22 15:59:05 +0530

    SumOfDigits

---
+++ src/Learning_Java/SumOfDigits.java
@@ -0,0 +1,26 @@
+package Learning_Java;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Enter the Number to Find the Sum of its Digits");		
+		int num = userInput.nextInt();
+		int temp = num;
+		int sum=0, rem=0;
+		
+		while(temp!=0) 
+		{   
+			rem = temp%10;
+			sum = sum + rem;
+			temp =  temp/10;
+	
+		}
+		System.out.println("Sum of Digits are: "+sum);
+      
+
+	}
+
+}
