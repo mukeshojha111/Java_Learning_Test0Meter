81ca993f630025cda76a3c735400de8a5aa5f416
Mukesh Ojha <Mukesh Ojha@MUKESH-PF2XRWFT-82lN>
2023-11-28 09:17:08 +0530

    3 Infinite loops

---
+++ src/Learning_Java/InfiniteLoop2.java
@@ -0,0 +1,28 @@
+package Learning_Java;
+
+public class InfiniteLoop2 {
+
+	public static void main(String[] args) {
+		// for Loop
+		
+		for (;;) {
+            
+        }
+		
+		/* while Loop
+		
+		 while (true) {
+	           
+	        }
+	*/
+		
+		
+		/* do while Loop
+		
+		  do {
+            // Code that will be executed indefinitely
+        } while (true);
+	*/
+	}
+
+}
