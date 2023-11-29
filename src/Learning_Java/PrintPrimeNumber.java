6de88d7e78020aa2c30b5c234376cf818f1d55e4
Mukesh Ojha <Mukesh Ojha@MUKESH-PF2XRWFT-82lN>
2023-11-29 07:28:37 +0530

    PrimeNumbers

---
+++ src/Learning_Java/PrintPrimeNumber.java
@@ -0,0 +1,29 @@
+package Learning_Java;
+
+public class PrintPrimeNumber {
+
+	public static void main(String[] args) {
+		// Prime Nos - A no which is divisible by 1 and itself is know as prime Number
+		
+		int temp =0;
+		System.out.print("20 Prime Numbers are: ");
+		for(int i=2;i<=72;i++)
+		{
+		     for(int j=2; j<=i-1;j++)
+		     {
+		    	if(i%j==0)
+		    	{
+		    		temp = temp+1;
+		    	}
+		     }
+		     if(temp==0)
+		     {
+		    	 System.out.print(i+"|");
+		     }
+		     else
+		     {
+		    	 temp=0;
+		     }
+		}
+	}
+}
