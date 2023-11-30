8d47a1c1b5db7dde8e2180c9cc8df436eb7cbe3f
Mukesh Ojha <Mukesh Ojha@MUKESH-PF2XRWFT-82lN>
2023-11-30 09:24:48 +0530

    Objects&Class

---
+++ src/Learning_Java/MultipleClasses.java
@@ -0,0 +1,109 @@
+package Learning_Java;
+
+public class MultipleClasses {
+
+}
+
+class Human {
+	String name;
+	int age;
+	String gender;
+
+}
+
+class Laptop {
+	String brand;
+	String model;
+	String processor;
+	int ramSize;
+
+}
+
+class SportMan {
+	String name;
+	String sport;
+	String team;
+	String position;
+
+}
+
+class TestCase {
+	String name;
+	String description;
+	String status;
+
+}
+
+// Additional Classes
+
+class Car {
+	String make;
+	String model;
+	int year;
+	String color;
+
+}
+
+class Book {
+	String title;
+	String author;
+	String genre;
+	int pages;
+
+}
+
+class Dog {
+	String name;
+	String breed;
+	int age;
+
+}
+
+class CoffeeCup {
+	String material;
+	String size;
+	String color;
+
+}
+
+class Movie {
+	String title;
+	String director;
+	String genre;
+	int releaseYear;
+
+}
+
+class Smartphone {
+	String brand;
+	String model;
+	String operatingSystem;
+	int storageCapacity;
+}
+
+class Teacher {
+	String name;
+	String subject;
+	int experienceYears;
+}
+
+class Restaurant {
+	String name;
+	String cuisine;
+	String location;
+	double rating;
+}
+
+class MusicalInstrument {
+	String type;
+	String brand;
+	String material;
+	String soundRange;
+}
+
+class Planet {
+	String name;
+	double diameter;
+	double distanceFromSun;
+	int numberOfMoons;
+}
