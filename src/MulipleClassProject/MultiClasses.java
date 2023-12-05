f3a5f28f5b09719bac1ade07adc7333168b2acf3
Mukesh Ojha <Mukesh Ojha@MUKESH-PF2XRWFT-82lN>
2023-12-05 08:21:54 +0530

    ......

---
+++ src/MulipleClassProject/MultiClasses.java
@@ -0,0 +1,169 @@
+package MulipleClassProject;
+
+public class MultiClasses {
+
+}
+
+class Human {
+	String name;
+	int age;
+	String gender;
+	
+	void run(){
+		
+	}
+
+}
+
+class Laptop {
+	String brand;
+	String model;
+	String processor;
+	String ramSize;
+	
+	void work() {
+		
+	}
+
+}
+
+class SportMan {
+	String name;
+	String sport;
+	String team;
+	String position;
+	
+	void play() {
+		
+	}
+
+}
+
+class TestCase {
+	String name;
+	String description;
+	String status;
+	
+	void scenario()
+	{
+		
+	}
+
+}
+
+// Additional Classes
+
+class Car {
+	String make;
+	String model;
+	int year;
+	String color;
+	
+	void drive()
+	{
+		
+	}
+
+}
+
+class Book {
+	String title;
+	String author;
+	String genre;
+	int pages;
+	
+	void read()
+	{
+		
+	}
+
+}
+
+class Dog {
+	String name;
+	String breed;
+	int age;
+
+	void bark()
+	{}
+}
+
+class CoffeeCup {
+	String material;
+	String size;
+	String color;
+	
+	void taste()
+	{
+		
+	}
+
+}
+
+class Movie {
+	String title;
+	String director;
+	String genre;
+	int releaseYear;
+	
+	void watch() {}
+
+}
+
+class Smartphone {
+	String brand;
+	String model;
+	String operatingSystem;
+	int storageCapacity;
+	
+	void call()
+	{
+		
+	}
+}
+
+class Teacher {
+	String name;
+	String subject;
+	int experienceYears;
+	
+	void Study()
+	{
+		
+	}
+}
+
+class Restaurant {
+	String name;
+	String cuisine;
+	String location;
+	double rating;
+	
+	void Dinning()
+	{
+		
+	}
+}
+
+class MusicalInstrument {
+	String type;
+	String brand;
+	String material;
+	String soundRange;
+	
+	void playingMusic() {
+		
+	}
+}
+
+class Planet {
+	String name;
+	double diameter;
+	double distanceFromSun;
+	int numberOfMoons;
+	
+	void rotation()
+	{
+		
+	}
+}
