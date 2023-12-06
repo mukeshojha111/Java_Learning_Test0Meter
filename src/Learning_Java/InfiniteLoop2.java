f075971c5ce8a89706d36bd00da6ceaf67480371
Mukesh Ojha <Mukesh Ojha@MUKESH-PF2XRWFT-82lN>
2023-12-06 07:41:02 +0530

    Updated...!!

--- src/Learning_Java/InfiniteLoop2.java
+++ src/Learning_Java/InfiniteLoop2.java
@@ -1,28 +1,71 @@
 package Learning_Java;
 
 public class InfiniteLoop2 {
-
-	public static void main(String[] args) {
-		// for Loop
-		
-		for (;;) {
+	
+	// FOR LOOP..............
+	
+	void loopForOne()
+	{
+         for (;;){
             
         }
-		
-		/* while Loop
-		
+	}
+	
+	void loopForTwo()
+	{
+		for (int i = 0; ; i++) {
+		    
+		}
+	}
+	
+	void loopForThree()
+	{
+		for (int i = 0; ; i++) {
+		    
+		}
+	}
+	
+// while Loop.................
+	
+	void loopWhileOne() {
 		 while (true) {
-	           
 	        }
-	*/
+	}
+	
+	
+	void loopWhileTwo() {
+		boolean keepRunning = true;
+		while (keepRunning) {
+		}
+	}
+	
+	void loopWhileThree() {
+//		while (No condition provided) 
+		{
+		    
+		}
+	}
+	
+// Do While..................................
+	
+	void loopDoWhileOne() {
+		 do {
+       
+    } while (true);
+	}
+
+	void loopDoWhileTwo() 
+	{
+		boolean keepRunning = true;
+		do {
+		  
+		} while (keepRunning);
+	}
+	
+	void loopDoWhileThree()
+	{
 		
-		
-		/* do while Loop
-		
-		  do {
-            // Code that will be executed indefinitely
-        } while (true);
-	*/
 	}
 
 }
+
