06a445d3b4812dbd704e97b9d7494d94210ddda7
Mukesh Ojha <Mukesh Ojha@MUKESH-PF2XRWFT-82lN>
2023-12-07 09:36:16 +0530

    Documentation

---
+++ src/MethodsPractices/DocumentationCalling.java
@@ -0,0 +1,15 @@
+package MethodsPractices;
+
+public class DocumentationCalling {
+
+
+		public static void main(String[] args) {
+	        // Create an instance of DocumentationExample
+	        DocumentationExample example = new DocumentationExample();
+
+	        // Call the commentMethod with values for parameters
+	        example.commentMethod(10, 20);
+
+	}
+
+}
---
+++ src/MethodsPractices/DocumentationExample.java
@@ -0,0 +1,16 @@
+package MethodsPractices;
+
+public class DocumentationExample {
+
+    /**
+     * This method is used to learn documentation comments in Java.
+     * <br/> This line is written for HTML presentation.
+     *
+     * @param parameter1 The first parameter.
+     * @param parameter2 The second parameter.
+     */
+    public void commentMethod(int parameter1, int parameter2) {
+   
+        System.out.println("Inside commentMethod with parameters: " + parameter1 + ", " + parameter2);
+    }
+}
