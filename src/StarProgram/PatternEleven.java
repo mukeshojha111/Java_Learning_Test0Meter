de6fe0e61d506988a917a3d1096c9319ff0ee2a7
Mukesh Ojha <Mukesh Ojha@MUKESH-PF2XRWFT-82lN>
2023-12-08 09:40:53 +0530

    Pattern 10-11

---
+++ src/StarProgram/PatternEleven.java
@@ -0,0 +1,24 @@
+package StarProgram;
+
+public class PatternEleven {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) {
+            // Print spaces before asterisks
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print asterisks
+            for (int k = 1; k <= 5; k++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line after printing each row
+            System.out.println();
+        }
+		
+		
+	}
+}
---
+++ src/StarProgram/PatternTen.java
@@ -0,0 +1,24 @@
+package StarProgram;
+
+public class PatternTen {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 6; i++) {
+            // Print spaces before asterisks
+            for (int j = 1; j <= 6 - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print asterisks
+            for (int k = 1; k <= 6; k++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line after printing each row
+            System.out.println();
+        }
+		
+		
+	}
+}
