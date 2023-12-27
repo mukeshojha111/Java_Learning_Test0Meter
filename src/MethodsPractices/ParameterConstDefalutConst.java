e4f6edc871b277c979ef95411d14018a5ee16710
Mukesh Ojha <Mukesh Ojha@MUKESH-PF2XRWFT-82lN>
2023-12-27 14:44:32 +0530

    ParameterConstDefalutConst

---
+++ src/MethodsPractices/ParameterConstDefalutConst.java
@@ -0,0 +1,18 @@
+package MethodsPractices;
+
+public class ParameterConstDefalutConst {
+
+	public static void main(String[] args) {
+		A1 x = new A1();
+	}
+
+}
+class A1{
+
+A1(){
+     this(1);
+}
+A1(int i){
+System.out.println("Paramaeterized Constructor");
+}
+}
