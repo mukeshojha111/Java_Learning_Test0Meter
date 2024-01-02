ee4fb271a8f2e27731cad1c0f62b0c07c105a8b6
Mukesh Ojha <Mukesh Ojha@MUKESH-PF2XRWFT-82lN>
2024-01-02 10:25:04 +0530

    CharactersInString

---
+++ src/StringOperations/CharacterInStrings.java
@@ -0,0 +1,13 @@
+package StringOperations;
+
+public class CharacterInStrings {
+
+	public static void main(String[] args) {
+		
+		String  s1 = "this is java";
+		String s2 = s1.replaceAll("this is java", "thisisjava");
+		System.out.println("Lenght of String: "+s2.length());
+
+	}
+
+}
