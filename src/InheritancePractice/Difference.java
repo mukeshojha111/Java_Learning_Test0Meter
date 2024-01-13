431e18d0bf9dbc03518958d84b745297560bf287
Mukesh <Mukesh@Maverick>
2024-01-13 08:24:21 +0530

    Difference Between 1. Inheritance and Encapsulation 2. Inheritance and Abstraction 3. Polymorphism and Inheritance 4.Composition and Inheritance

---
+++ src/InheritancePractice/Difference.java
@@ -0,0 +1,43 @@
+package InheritancePractice;
+
+public class Difference {
+	
+/**
+ 1. Inheritance and Encapsulation:
+
+Inheritance: It is a mechanism in object-oriented programming that allows a Sub class (child class) to inherit properties and behaviors from Super class (Parent class). 
+It promotes code reuse and establishes an "is-a" relationship between classes.
+
+Encapsulation: It is a concept that involves bundling the data (attributes) and the methods (functions) that operate on the data into a single unit known as a class. 
+Encapsulation helps in data hiding and protecting the internal state of an object.
+
+------------------------------------------------------------------------------------------
+
+2. Inheritance and Abstraction:
+
+Inheritance: It is a mechanism in object-oriented programming that allows a Sub class (child class) to inherit properties and behaviors from Super class (Parent class). 
+It promotes code reuse and establishes an "is-a" relationship between classes.
+
+Abstraction: It is a concept that focuses on hiding the complex implementation details and showing only the essential features of an object. 
+Abstraction is often achieved through abstract classes and interfaces, allowing for the definition of common interfaces without specifying the exact implementation.
+
+------------------------------------------------------------------------------------------
+
+3. Polymorphism and Inheritance:
+
+Polymorphism: It is the ability of a single function or method to operate on different types of data or objects. 
+
+There are two types: compile-time (method overloading) and runtime (method overriding).
+
+Inheritance: It is a mechanism in object-oriented programming that allows a Sub class (child class) to inherit properties and behaviors from Super class (Parent class). 
+It promotes code reuse and establishes an "is-a" relationship between classes.
+
+--------------------------------------------------------------------------------------------
+
+4.Composition and Inheritance:
+Composition: It involves creating complex objects by combining simpler ones. Instead of relying on a hierarchical relationship, composition uses relationships between classes where one class contains an instance of another class.
+Inheritance: It is a mechanism in object-oriented programming that allows a Sub class (child class) to inherit properties and behaviors from Super class (Parent class). 
+It promotes code reuse and establishes an "is-a" relationship between classes. 
+ */
+
+}
