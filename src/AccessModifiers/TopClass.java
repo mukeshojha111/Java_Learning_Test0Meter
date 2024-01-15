23464afaf1d4426f30c6ef3db873b973937f83d0
Mukesh <Mukesh@Maverick>
2024-01-15 08:43:03 +0530

    TopClass

---
+++ src/AccessModifiers/TopClass.java
@@ -0,0 +1,24 @@
+package AccessModifiers;
+
+public class TopClass {
+
+	/**
+	 Nested or Inner class cannot have access modifier as private or protected. 
+	 */
+}
+
+class TopClass1{
+	
+}
+
+// getting compilation error if we marking class as private or protected.
+
+/* private class TopClass2{   
+	
+}
+
+protected class TopClass2{
+	
+}
+
+*/
