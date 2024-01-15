1b9dcdff20a3e631f49bfc81acfe01aa7f041d44
Mukesh <Mukesh@Maverick>
2024-01-15 08:36:30 +0530

    default variable in java interface

---
+++ src/Interfaces/Interface1.java
@@ -0,0 +1,13 @@
+package Interfaces;
+
+/**
+  Default variable types in Interface are public, final & static.
+ */
+
+public interface Interface1 {
+     
+	int i = 10;
+	final Double j =20.33;
+	static String s = "interface";
+	
+}
