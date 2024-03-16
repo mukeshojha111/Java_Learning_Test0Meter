fdd7778e08a618f1a86920939ca3a7ebf984ecd6
Mukesh <Mukesh@DESKTOP-7VL5O7M>
2024-03-16 10:35:36 +0530

    StringPer Updated..!!

--- src/StringOperations/StringPermutations.java
+++ src/StringOperations/StringPermutations.java
@@ -3,25 +3,40 @@ package StringOperations;
 import java.util.Scanner;
 
 public class StringPermutations {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter a string:");
-        String input = scanner.nextLine();
-
-        System.out.println("Permutations of the string:");
-        printPermutations("", input);
+	public static void main(String[] args) {
+        String input = "1253";
+        printPermutations(input);
     }
 
-    private static void printPermutations(String prefix, String remaining) {
-        int n = remaining.length();
-        if (n == 0) {
-            System.out.println(prefix);
-        } else {
-            for (int i = 0; i < n; i++) {
-                printPermutations(prefix + remaining.charAt(i), remaining.substring(0, i) + remaining.substring(i + 1, n));
+    public static void printPermutations(String str) {
+        char[] chars = str.toCharArray();
+        int n = chars.length;
+        int[] indexes = new int[n];
+        int i = 0;
+
+        System.out.println(str); // Print the original string
+
+        while (i < n) {
+            if (indexes[i] < i) {
+                if (i % 2 == 0) {
+                    swap(chars, 0, i);
+                } else {
+                    swap(chars, indexes[i], i);
+                }
+                System.out.println(new String(chars));
+                indexes[i]++;
+                i = 0;
+            } else {
+                indexes[i] = 0;
+                i++;
             }
         }
+    }
+
+    public static void swap(char[] chars, int i, int j) {
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
     }
 }
 
